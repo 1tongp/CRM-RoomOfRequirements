22d34e14e1674cae66b83dd5fcfbdf9aa4b36b7d
[Yitong <yitong0822@gmail.com>
2021-08-29 12:18:39 +1000

    implement the connection between frontend and backend

--- customer/src/Customer/Customer.js
+++ customer/src/Customer/Customer.js
@@ -1,12 +1,12 @@
 import axios from '../API/axios';
 import Navigation from '../components/Navigation';
-
+import CustomerList from '../components/CustomerList';
 function Customer(props) {
     console.log(props)
     return (
         <div>
             <Navigation data = {props}></Navigation>
-            <p>customer page</p>
+            <CustomerList/>
             
         </div>
     )
--- customer/src/Dashboard/Dashboard.js
+++ customer/src/Dashboard/Dashboard.js
@@ -1,18 +1,33 @@
-
+import { Layout } from 'antd';
 import axios from '../API/axios';
 import Navigation from '../components/Navigation';
+import '../components/component.css';
 
+const { Header, Content } = Layout;
 function Dashboard(props) {
     console.log(props)
     return (
-        <div>
-            <Navigation data = {props}></Navigation>
-            {/* <p>dashboard information from props</p>
-            <p>staff familyName: {props.location.state.staff.familyName}</p>
-            <p>staff id: {props.location.state.staff.id}</p>
-            <p>staff loginEmail: {props.location.state.staff.loginEmail}</p>
-            <p>staff password: {props.location.state.staff.password}</p> */}
-        </div>
+        <>
+            <Navigation data={props}></Navigation>
+
+            <Layout>
+                <Content className="site-layout-background-content">
+                    <div className="container--customerlist">
+                        <div className="container--basicinfo">
+                            <p>dashboard information from props</p>
+                            <p>staff familyName: {props.location.state.staff.familyName}</p>
+                            <p>staff id: {props.location.state.staff.id}</p>
+                            <p>staff loginEmail: {props.location.state.staff.loginEmail}</p>
+                            <p>staff password: {props.location.state.staff.password}</p>
+                        </div>
+                    </div>
+                </Content>
+            </Layout>
+        </>
+
+
+
+
     )
 }
 
