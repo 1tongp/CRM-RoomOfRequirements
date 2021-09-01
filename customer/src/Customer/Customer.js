0fb44e28323bc1297e141b353fc446c4d7f19636
[Yitong <yitong0822@gmail.com>
2021-09-01 19:39:00 +1000

    connected the customer page with nav bar

--- customer/src/Customer/Customer.js
+++ customer/src/Customer/Customer.js
@@ -2,20 +2,38 @@ import axios from '../API/axios';
 import Navigation from '../components/Navigation';
 import CustomerList from '../components/CustomerList';
 import '../components/component.css'
-function Customer(props) {
-    console.log(props)
-    return (
-        <div className='div'>
-            <div className='navigationBar'>
-                <Navigation data={props}></Navigation>
+import React , {Component} from "react";
+
+class Customer extends React.Component {
+    constructor(props) {
+        super(props);
+        console.log(this.props)
+    }
+
+    state = {
+        collapsed: false,
+    };
+
+    toggle = () => {
+        this.setState({
+            collapsed: !this.state.collapsed,
+        });
+    };
+
+    render() {
+        return (
+            <div className='div'>
+                <div className='navigationBar'>
+                    <Navigation data={this.props}></Navigation>
+                </div>
+                <div className='customerList'>
+                    <CustomerList />
+                </div>
+
+
             </div>
-            <div className='customerList'>
-                <CustomerList />
-            </div>
-            
-            
-        </div>
-    )
+        );
+    }
 }
 
 export default Customer;
