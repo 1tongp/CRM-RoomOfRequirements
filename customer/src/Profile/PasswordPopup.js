583cb63f2b1667a49155519b98ca14b4489c4f44
Coco-byte <69224442+Coco-byte@users.noreply.github.com>
2021-09-05 13:12:04 +1000

    popup solving

--- customer/src/Profile/PasswordPopup.js
+++ customer/src/Profile/PasswordPopup.js
@@ -1,13 +1,30 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import './Profile.css';
+import { Button, Modal, Form, Row, Col} from 'react-bootstrap';
+
 
 function PasswordPopup(props) {
     return (props.trigger) ? (
         <div className="popUp">
             <div className="popUp-inner">
-                <button className="close-btn" onClick={() => props.setState(false)}>Save and close</button>
-                {props.children}
+                <div className="popUp-title">Change Password for account: "xxx@hsbc.com"</div>
+                <Form>
+                    <Col>
+                    <Form.Group as={Col} controlId="formGridOldPassword">
+                    <Form.Label>New Password</Form.Label>
+                    <Form.Control size="lg" type="text" placeholder="Enter Your New Password" />
+                    </Form.Group>
+                    </Col>
+                    <Col>
+                    <Form.Group as={Col} controlId="formGridNewPassword">
+                    <Form.Label>Old Password</Form.Label>
+                    <Form.Control size="lg" type="text" placeholder="Enter Your Old Password" />
+                    </Form.Group>
+                    </Col>
+                </Form>
+                <button className="close-btn" onClick={() => props.setTrigger(false)}>Save and close</button>
+                {/* {props.children} */}
             </div>
         </div>
 
