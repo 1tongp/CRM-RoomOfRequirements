f86e05f1d86f8dff260eb2840086a12e9fce7416
Xinyi-Y <80232401+Xinyi-Y@users.noreply.github.com>
2021-09-06 18:00:57 +0800

    add new functions and update the server for chat functionality

--- server.js
+++ server.js
@@ -22,6 +22,9 @@ const order = require('./routes/order');
 const team = require('./routes/team');
 const calendar = require('./routes/calendar');
 
+// functions for chat update
+const { addUser, removeUser, getUser, getUsersInRoom } = require('./users');
+
 var cors = require('cors')
 app.use(cors());
 app.get('/', (req, res) => {
@@ -30,12 +33,50 @@ app.get('/', (req, res) => {
 // Bodyparser Middleware
 app.use(bodyParser.json());
 
+// chat
+io.on('connect', (socket) => {
+  socket.on('join', ({ name, room }, callback) => {
+    const { error, user } = addUser({ id: socket.id, name, room });
+
+    if(error) return callback(error);
+
+    socket.join(user.room);
+
+    socket.emit('message', { user: 'admin', text: `${user.name}, welcome to room ${user.room}.`});
+    socket.broadcast.to(user.room).emit('message', { user: 'admin', text: `${user.name} has joined!` });
+
+    io.to(user.room).emit('roomData', { room: user.room, users: getUsersInRoom(user.room) });
+
+    callback();
+  });
+
+  socket.on('sendMessage', (message, callback) => {
+    const user = getUser(socket.id);
+
+    io.to(user.room).emit('message', { user: user.name, text: message });
+
+    callback();
+  });
+
+  socket.on('disconnect', () => {
+    const user = removeUser(socket.id);
+
+    if(user) {
+      io.to(user.room).emit('message', { user: 'Admin', text: `${user.name} has left.` });
+      io.to(user.room).emit('roomData', { room: user.room, users: getUsersInRoom(user.room)});
+    }
+  })
+});
+
+
+/*
 // chat starts here
 io.on('connection', socket => {
   socket.on('message', ({ name, message }) => {
     io.emit('message', { name, message })
   })
 })
+*/
 
 
 // io.of("api/socket").on("connection", (socket) => {
