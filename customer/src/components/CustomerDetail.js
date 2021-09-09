a40f9e7bce33d404f3e9eb4561f9a8cc2e4acc21
[Yitong <yitong0822@gmail.com>
2021-09-09 15:48:21 +1000

    connected with the backend! needs to implememnt the front-end format

--- customer/src/components/CustomerDetail.js
+++ customer/src/components/CustomerDetail.js
@@ -1,7 +1,24 @@
 import React, { useState } from 'react';
 import { Modal, Button } from 'antd';
 import './Customer.css';
-const CustomerDetail = (props) => {
+import { useEffect } from 'react';
+import axios from '../API/axios.js';
+
+function CustomerDetail (props){
+  // console.log(props);
+
+  const [detail, setData] = useState([])
+    useEffect(
+        () => {
+            axios.get('/order/customertype/' + props.data.id + '?type=' + props.data.insurance).then(response => {
+                if (response.data.success) {
+                    setData(response.data.orderDetail[0])
+                    
+                }
+            })
+			
+        }, [])
+
   const [isModalVisible, setIsModalVisible] = useState(false);
 
   const showModal = () => {
@@ -15,25 +32,32 @@ const CustomerDetail = (props) => {
   const handleCancel = () => {
     setIsModalVisible(false);
   };
-  console.log(props.detailInfo.length);
-  var customerList = [];
-  for (let i = 0; i < props.detailInfo.length; i++){
-    customerList.push(props.detailInfo[i]);
-  }
+  // console.log(props.detailInfo.length);
+  // var customerList = [];
+  // for (let i = 0; i < props.detailInfo.length; i++){
+  //   customerList.push(props.detailInfo[i]);
+  // }
   
 
-  console.log(customerList);
-
+  // console.log(customerList);
+  
 
   return (
     <>
       <Button className='button' onClick={showModal}>
         Details
       </Button>
-      <Modal title="Basic Modal" visible={isModalVisible} onOk={handleOk} onCancel={handleCancel}>
+      <Modal title="DETAILS" visible={isModalVisible} onOk={handleOk} onCancel={handleCancel}>
         {/* <p>{props.detailInfo}</p> */}
-        <p>Some contents...</p>
-        <p>Some contents...</p>
+        <p>Customer Name: {props.data.firstName} {props.data.lastName}</p>
+        <p>Customer ID: {props.data.id}</p>
+        <p>Insurance Order ID: {detail._id}</p>
+        <p>Insurance Type: {detail.type}</p>
+        <p>Insurance Status: {detail.status}</p>
+        <p>Purchase Time: {detail.createTime}</p>
+        <p>Expire Date: {detail.expireDate}</p>
+        <p>Order Details: {detail.detail}</p>
+
       </Modal>
     </>
   );
