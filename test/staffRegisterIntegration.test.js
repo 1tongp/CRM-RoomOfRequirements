6ff72bb5774c591ea8b413518df48e4142bb0a6d
XiaochenHou <xihou@student.unimelb.edu.au>
2021-09-23 15:26:15 +0800

    31 ci/cd test

--- test/staffRegisterIntegration.test.js
+++ test/staffRegisterIntegration.test.js
@@ -8,7 +8,7 @@ const testStaffSignup = {
     vaildBody: {
         "givenName": "Kerry1",
         "familyName": "liu1",
-        "loginEmail": "26@test",
+        "loginEmail": "31@test",
         "password": "365",
         "role": "manager",
         "phone": 13580807979
