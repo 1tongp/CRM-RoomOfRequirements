9d70106a87a1ec7f102b6747e1624dfbd60ff63a
XiaochenHou <xihou@student.unimelb.edu.au>
2021-10-04 20:19:02 +0800

    add history form

--- customer/src/Dashboard/Chart.js
+++ customer/src/Dashboard/Chart.js
@@ -1,68 +1,67 @@
-import React from 'react';
-import { Line } from '@ant-design/charts';
-import { Bar } from '@ant-design/charts';
+import React from "react";
+import { Line } from "@ant-design/charts";
+import { Bar } from "@ant-design/charts";
 
 export function Chart() {
-  const data = [
-    { year: '1991', value: 3 },
-    { year: '1992', value: 4 },
-    { year: '1993', value: 3.5 },
-    { year: '1994', value: 5 },
-    { year: '1995', value: 4.9 },
-    { year: '1996', value: 6 },
-    { year: '1997', value: 7 },
-    { year: '1998', value: 9 },
-    { year: '1999', value: 13 },
-  ];
+    const data = [
+        { year: "1991", value: 3 },
+        { year: "1992", value: 4 },
+        { year: "1993", value: 3.5 },
+        { year: "1994", value: 5 },
+        { year: "1995", value: 4.9 },
+        { year: "1996", value: 6 },
+        { year: "1997", value: 7 },
+        { year: "1998", value: 9 },
+        { year: "1999", value: 13 },
+    ];
 
-  const config = {
-    data,
-    height: 400,
-    xField: 'year',
-    yField: 'value',
-    point: {
-      size: 5,
-      shape: 'diamond',
-    },
-    label: {
-      style: {
-        fill: '#aaa',
-      },
-    },
-  };
-  return <Line {...config} />;
-};
-
+    const config = {
+        data,
+        height: 400,
+        xField: "year",
+        yField: "value",
+        point: {
+            size: 5,
+            shape: "diamond",
+        },
+        label: {
+            style: {
+                fill: "#aaa",
+            },
+        },
+    };
+    return <Line {...config} />;
+}
 
 export function DemoBar() {
-  var data = [
-    {
-      year: '1951',
-      value: 38,
-    },
-    {
-      year: '1952',
-      value: 52,
-    },
-    {
-      year: '1956',
-      value: 61,
-    },
-    {
-      year: '1957',
-      value: 145,
-    },
-    {
-      year: '1958',
-      value: 48,
-    },
-  ];
-  var config = {
-    data: data,
-    xField: 'value',
-    yField: 'year',
-    seriesField: 'year',
-    legend: { position: 'top-left' },
-  };
-  return <Bar {...config} />;
-};
+    var data = [
+        {
+            year: "1951",
+            value: 38,
+        },
+        {
+            year: "1952",
+            value: 52,
+        },
+        {
+            year: "1956",
+            value: 61,
+        },
+        {
+            year: "1957",
+            value: 145,
+        },
+        {
+            year: "1958",
+            value: 48,
+        },
+    ];
+    var config = {
+        data: data,
+        xField: "value",
+        yField: "year",
+        seriesField: "year",
+        legend: { position: "top-left" },
+    };
+    return <Bar {...config} />;
+}
--- customer/src/Dashboard/DashboardTable.js
+++ customer/src/Dashboard/DashboardTable.js
@@ -1,61 +1,60 @@
-import { Table, Tag, Space } from 'antd';
-import React from 'react'
+import { Table, Tag, Space } from "antd";
+import React from "react";
 const { Column, ColumnGroup } = Table;
 
-
-
 export default function DashboardTable(props) {
-console.log(props);
-  const data = [
-    {
-      key: '1',
-      insurance: "1",
-      tags: ['woman', 'young'],
-    },
-    {
-      key: '2',
-      insurance: "2",
-      tags: ['man', 'old'],
-    },
-    {
-      key: '3',
-      insurance: "3",
-      tags: ['man', 'young'],
-    },
-  ];
+    console.log(props);
+    const data = [
+        {
+            key: "1",
+            insurance: "1",
+            tags: ["woman", "young"],
+        },
+        {
+            key: "2",
+            insurance: "2",
+            tags: ["man", "old"],
+        },
+        {
+            key: "3",
+            insurance: "3",
+            tags: ["man", "young"],
+        },
+    ];
 
     return (
         <div>
             <Table dataSource={data}>
-                <Column title="Insurance" dataIndex="insurance" key="insurance" />
                 <Column
-                title="Tags"
-                dataIndex="tags"
-                key="tags"
-                render={tags => (
-                    <>
-                    {tags.map(tag => (
-                        <Tag color="blue" key={tag}>
-                        {tag}
-                        </Tag>
-                    ))}
-                    </>
-                )}
+                    title="Insurance"
+                    dataIndex="insurance"
+                    key="insurance"
                 />
-            
-            <Column
-                title="Action"
-                key="action"
-                render={(text, record) => (
-                    <Space size="middle">
-                    <a>View Product Detail</a>
-                    </Space>
-                )}
+                <Column
+                    title="Tags"
+                    dataIndex="tags"
+                    key="tags"
+                    render={(tags) => (
+                        <>
+                            {tags.map((tag) => (
+                                <Tag color="blue" key={tag}>
+                                    {tag}
+                                </Tag>
+                            ))}
+                        </>
+                    )}
+                />
+
+                <Column
+                    title="Action"
+                    key="action"
+                    render={(text, record) => (
+                        <Space size="middle">
+                            <a>View Product Detail</a>
+                        </Space>
+                    )}
                 />
             </Table>
-
         </div>
-    )
+    );
 }
-
-  
